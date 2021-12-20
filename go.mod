89aed1f58f5ffbeb39db077d9b56668ae5a53d81
RestartFU <zerkoming@gmail.com>
2021-12-19 21:07:14 -0800

    update ranks

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/SGPractice/glow-bot
 go 1.18
 
 require (
-	github.com/SGPractice/link v0.0.0-20211216230834-c9242c14266a
+	github.com/SGPractice/link v0.0.0-20211219191312-e9bbeedf5d4a
 	github.com/bwmarrin/discordgo v0.23.2
 	github.com/go-sql-driver/mysql v1.6.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -14,9 +16,45 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	session.Identify.Intents = discordgo.IntentsGuildMembers
 	if err := session.Open(); err != nil {
 		log.Fatalln(err)
 	}
+	ranks := map[string]string{
+		"Zeus":   "922234208787759125",
+		"Kratos": "922234180870492301",
+		"Triton": "922234161253716029",
+		"Hermes": "922234150130430013",
+		"Ares":   "922234108330008656",
+	}
+	go func() {
+		var after string
+		ticker := time.NewTicker(1 * time.Minute / 2)
+		for {
+			<-ticker.C
+			members, _ := session.GuildMembers("914172147520401448", after, 1000)
+			if len(members) <= 0 {
+				after = ""
+			}
+			for _, m := range members {
+				if r, ok, _ := linker.LinkedFromDiscordID(m.User.ID); ok {
+					fmt.Println(r.Username())
+					if rows, err := linker.DB().Query(fmt.Sprintf("SELECT role FROM playerdata WHERE username='%s';", r.Username())); err == nil {
+						var role string
+						for rows.Next() {
+							rows.Scan(&role)
+						}
+						rank, ok := ranks[role]
+						fmt.Println(role, rank)
+						if ok {
+							session.GuildMemberRoleAdd("914172147520401448", m.User.ID, rank)
+						}
+					}
+				}
+				after = m.User.ID
+			}
+		}
+	}()
 	session.AddHandler(MessageCreate)
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
